84ae74dc73ae535c07a2af5ce9bcbd65d8261869
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-03-01 15:05:10 +0600

    showing two line description of card description

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const cardArray = [
-    { id: 0, subTitlte: "ShopEase", title: 'Fix Mobile Button Issue ', description: "Fix mobile button issues by ensuring responsive design, using correct CSS styles, touch-friendly sizes, and debugging with browser developer tools.", date: "21 March 2025" },
-    { id: 1, subTitlte: "CloudSync", title: 'Add Dark Mode', description: "Add dark mode using CSS variables, media queries, or JavaScript toggle button to switch themes dynamically for better user experience.", date: "21 March 2025" },
-    { id: 2, subTitlte: "SwiftPay", title: 'Optimize  Home page ', description: "DOptimize the homepage by improving load speed, using efficient images, minimizing CSS/JS, enabling caching, optimizing SEO, and ensuring responsive design.", date: "21 March 2025" },
-    { id: 3, subTitlte: "Meta", title: 'Add new emoji 🤲', description: "Add new emojis using Unicode characters, custom images, or emoji libraries in apps, websites, or messaging platforms for better user engagement.", date: "21 March 2025" },
-    { id: 4, subTitlte: "Google LLC", title: 'Integrate OpenAI API ', description: "Integrate OpenAI API by obtaining an API key, making HTTP requests, handling responses, and implementing features like chatbots, content generation, or automation.", date: "21 March 2025" },
-    { id: 5, subTitlte: "Glassdoar", title: 'Improve Job searching ', description: "Improve job searching by optimizing resumes, networking, using job boards, enhancing skills, tailoring applications, and staying updated on industry trends.", date: "21 March 2025" },
+    { id: 0, subTitlte: "ShopEase", title: 'Fix Mobile Button Issue ', description: "Fix mobile button issues by  ensuring <br> responsive design, using correct CSS styles, touch-friendly sizes, and debugging with browser developer tools.", date: "21 March 2025" },
+    { id: 1, subTitlte: "CloudSync", title: 'Add Dark Mode', description: "Add dark mode using CSS variables, <br> media queries, or JavaScript toggle button to switch themes dynamically for better user experience.", date: "21 March 2025" },
+    { id: 2, subTitlte: "SwiftPay", title: 'Optimize  Home page ', description: "DOptimize the homepage by improving <br> load speed, using efficient images, minimizing CSS/JS, enabling caching, optimizing SEO, and ensuring responsive design.", date: "21 March 2025" },
+    { id: 3, subTitlte: "Meta", title: 'Add new emoji 🤲', description: "Add new emojis using Unicode <br> characters, custom images, or emoji libraries in apps, websites, or messaging platforms for better user engagement.", date: "21 March 2025" },
+    { id: 4, subTitlte: "Google LLC", title: 'Integrate OpenAI API ', description: "Integrate OpenAI API by obtaining <br> an API key, making HTTP requests, handling responses, and implementing features like chatbots, content generation, or automation.", date: "21 March 2025" },
+    { id: 5, subTitlte: "Glassdoar", title: 'Improve Job searching ', description: "Improve job searching by optimizing <br> resumes, networking, using job boards, enhancing skills, tailoring applications, and staying updated on industry trends.", date: "21 March 2025" },
 ]
 
 
